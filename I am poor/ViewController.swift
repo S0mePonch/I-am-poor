5d4fd3f77474acd88e5842497e28fb5154bd294b
Alfonso Cano Vargas <yveltal02341@outlook.com>
2023-01-14 15:00:52 -0800

    Initial Commit

---
+++ I am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am poor
+//
+//  Created by Alfonso on 24/10/1401 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
